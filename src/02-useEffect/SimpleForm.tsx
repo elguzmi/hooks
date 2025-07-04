f362e1710e0a67e2e1e40d09a2cf9a18e2ffc294
Cesar Santiago Guzman Prada <santiago.guzman@parceiro.niw.pt>
2025-07-04 17:05:23 -0500

    adding simple form and event

---
+++ src/02-useEffect/SimpleForm.tsx
@@ -0,0 +1,28 @@
+import { ChangeEvent, useState } from "react"
+
+
+export const SimpleForm = () => {
+
+  const [formState, setFormState] = useState({
+    username:'ss',
+    email:''
+   });
+
+  const {email, username} = formState;
+
+  const onInputChange = (event:ChangeEvent)=>{
+    console.log(event.target)
+    //console.log(event.target.value , event.target.name)
+  }
+   
+  return (
+    <>
+      <h1>Simple Form</h1>
+      <hr />
+
+      <input type="text" className="form-control" placeholder="username" name="username" value={username} onChange={onInputChange} />
+
+      <input type="email" className="form-control mt-2" placeholder="santiago@hotmail.com" name="email" value={email} />
+    </>
+  )
+}
